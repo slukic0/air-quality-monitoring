dac3b0062af81d27804ebdc93313cbc5db4df520
Stefan Lukic <stefanlukic000@gmail.com>
2024-01-09 08:42:58 -0500

    add example secrets to X8 src

---
+++ MonitoringKit/Monitoring_X8/src/secrets.example.h
@@ -0,0 +1,42 @@
+#include <pgmspace.h>
+#define SECRET
+ 
+// WPA2 - PSK
+#define AP_SSID     "*****"
+#define PASSWORD    "*****"
+
+// WPA2 - Enterprise
+#define AP_SSID_E   "*****"
+#define ID_E        "*****"
+#define USERNAME_E  "*****"
+#define PASSWORD_E  "*****"
+
+#define THINGNAME "*****"
+
+const char AWS_IOT_ENDPOINT[] = "*****";
+
+// Amazon Root CA 1
+static const char AWS_CERT_CA[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+
+-----END CERTIFICATE-----
+)EOF";
+ 
+// Device Certificate
+static const char AWS_CERT_CRT[] PROGMEM = R"KEY(
+-----BEGIN CERTIFICATE-----
+
+-----END CERTIFICATE-----
+
+ 
+ 
+)KEY";
+ 
+// Device Private Key
+static const char AWS_CERT_PRIVATE[] PROGMEM = R"KEY(
+-----BEGIN RSA PRIVATE KEY-----
+
+-----END RSA PRIVATE KEY-----
+ 
+ 
+)KEY";
