2ff70aaabb26cffad1f5638375bb541fa8f46c10
Stefan Lukic <stefanlukic000@gmail.com>
2024-03-09 14:23:53 -0500

    get download UI mostly working

--- air-quality-sst/packages/web/src/pages/download.js
+++ air-quality-sst/packages/web/src/pages/download.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import { Box, Container, Stack, Typography, Grid, Paper } from '@mui/material';
 import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
 import { Chart } from 'src/components/chart';
 import { alpha, useTheme } from '@mui/material/styles';
@@ -11,6 +10,21 @@ import {
   getDeviceAggregateDataChartData,
   deviceMetrics,
 } from 'src/api/devices';
+import {
+  Box,
+  Container,
+  Stack,
+  Typography,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Grid,
+  TextField,
+} from '@mui/material';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const Page = () => {
   const { user } = useAuth();
@@ -19,28 +33,31 @@ const Page = () => {
     [user]
   );
 
-  const [device, setDevice] = useState('');
-  const [period, setPeriod] = useState('');
   const [deviceData, setDeviceData] = useState({ x: [], y: [] });
+
+  const [device, setDevice] = useState('');
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState();
 
   const onDeviceSelectorChange = (value) => {
     setDevice(value);
   };
-  const onPeriodSelectorChange = (value) => {
-    setPeriod(value);
+
+  const handleSubmit = () => {
+    console.log('Selected Device:', device);
+    console.log('Start Date:', startDate);
+    console.log('End Date:', endDate);
   };
 
-  // Get device data
-  useEffect(() => {
-    const getDeviceData = async () => {
-      if (device === '' || period === '') {
-        return;
-      }
-      const data = await getDeviceAggregateDataChartData(user.token, device, period);
-      setDeviceData(data);
-    };
-    getDeviceData();
-  }, [device, period, user.token]);
+  const lastDate = new Date();
+  const minTime = new Date();
+  minTime.setHours(0);
+  minTime.setMinutes(0);
+  minTime.setSeconds(0);
+
+  const limitStartTime = !startDate || startDate.getDay() === lastDate.getDay();
+  const limitEndTime = !endDate || endDate.getDay() === lastDate.getDay();
+  const isSubmitDisabled = !startDate || !endDate || !device;
 
   return (
     <>
@@ -65,22 +82,63 @@ const Page = () => {
               </Typography>
             </div>
             <div sx={{ p: 4 }}>
-              <Grid container spacing={2}>
-                <Grid item sm={6} xs={12}>
-                  <Selector
-                    defaultText={'Select Device'}
-                    items={devices}
-                    onChange={onDeviceSelectorChange}
-                  />
+              <FormControl>
+                <Grid container spacing={1}>
+                  <Grid item sm={6} xs={12}>
+                    <Selector
+                      defaultText={'Select Device'}
+                      items={devices}
+                      onChange={onDeviceSelectorChange}
+                    />
+                  </Grid>
+                  <Grid item md={6} xs={12}>
+                    <Grid container direction="column" alignItems="center" justifyContent="center">
+                      <Grid item xs={12}>
+                        <DatePicker
+                          maxWidth
+                          selected={startDate}
+                          onChange={(date) => setStartDate(date)}
+                          selectsStart
+                          startDate={startDate}
+                          endDate={endDate}
+                          placeholderText="Start Date"
+                          showTimeSelect
+                          dateFormat="MMMM d, yyyy h:mm aa"
+                          minTime={limitStartTime ? minTime : undefined}
+                          maxTime={limitStartTime ? lastDate : undefined}
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <DatePicker
+                          maxWidth
+                          selected={endDate}
+                          onChange={(date) => setEndDate(date)}
+                          selectsEnd
+                          startDate={startDate}
+                          endDate={endDate}
+                          placeholderText="End Date"
+                          showTimeSelect
+                          dateFormat="MMMM d, yyyy h:mm aa"
+                          minTime={limitEndTime ? minTime : undefined}
+                          maxTime={limitEndTime ? lastDate : undefined}
+                          minDate={startDate}
+                          maxDate={lastDate}
+                        />
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={handleSubmit}
+                      disabled={isSubmitDisabled}
+                    >
+                      Submit
+                    </Button>
+                  </Grid>
                 </Grid>
-                <Grid item sm={6} xs={12}>
-                  <Selector
-                    defaultText={'Select Period'}
-                    items={deviceAggregateDataPeriods}
-                    onChange={onPeriodSelectorChange}
-                  />
-                </Grid>
-              </Grid>
+              </FormControl>
             </div>
           </Stack>
         </Container>
