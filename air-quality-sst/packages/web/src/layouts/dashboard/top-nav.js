ca4c577bc0f140cb11eff9c04cd033b26939983b
Filip Lukic <filiplukic000@gmail.com>
2024-03-19 11:52:45 -0400

    Remove unused icons in nav bar

--- air-quality-sst/packages/web/src/layouts/dashboard/top-nav.js
+++ air-quality-sst/packages/web/src/layouts/dashboard/top-nav.js
@@ -48,57 +48,23 @@ export const TopNav = (props) => {
         <Stack
           alignItems="center"
           direction="row"
-          justifyContent="space-between"
+          justifyContent="flex-end"
           spacing={2}
           sx={{
             minHeight: TOP_NAV_HEIGHT,
             px: 2,
           }}
         >
-          <Stack alignItems="center" direction="row" spacing={2}>
-            {!lgUp && (
-              <IconButton onClick={onNavOpen}>
-                <SvgIcon fontSize="small">
-                  <Bars3Icon />
-                </SvgIcon>
-              </IconButton>
-            )}
-            <Tooltip title="Search">
-              <IconButton>
-                <SvgIcon fontSize="small">
-                  <MagnifyingGlassIcon />
-                </SvgIcon>
-              </IconButton>
-            </Tooltip>
-          </Stack>
-          <Stack alignItems="center" direction="row" spacing={2}>
-            <Tooltip title="Contacts">
-              <IconButton>
-                <SvgIcon fontSize="small">
-                  <UsersIcon />
-                </SvgIcon>
-              </IconButton>
-            </Tooltip>
-            <Tooltip title="Notifications">
-              <IconButton>
-                <Badge badgeContent={4} color="success" variant="dot">
-                  <SvgIcon fontSize="small">
-                    <BellIcon />
-                  </SvgIcon>
-                </Badge>
-              </IconButton>
-            </Tooltip>
-            <Avatar
-              onClick={accountPopover.handleOpen}
-              ref={accountPopover.anchorRef}
-              sx={{
-                cursor: 'pointer',
-                height: 40,
-                width: 40,
-              }}
-              src={user ? user.picture : null}
-            />
-          </Stack>
+          <Avatar
+            onClick={accountPopover.handleOpen}
+            ref={accountPopover.anchorRef}
+            sx={{
+              cursor: 'pointer',
+              height: 40,
+              width: 40,
+            }}
+            src={user ? user.picture : null}
+          />
         </Stack>
       </Box>
       <AccountPopover
